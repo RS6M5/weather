9e29a6ab1ddf371605bc76db9286acfdc1c91c29
RS6M5 <163847599+RS6M5@users.noreply.github.com>
2024-06-28 13:49:12 +0300

    add code

--- botWeather.py
+++ botWeather.py
@@ -4,11 +4,13 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
+from googletrans import Translator
+import os
 
 API_TOKEN = '7234232452:AAHzYMJe7GtlBYUa0Z4q_b8p3VY5tpwMPzg'
-WEATHER_API_KEY = '7473e21ea89305be3320287150c9407a'
+WEATHER_API_KEY = 'ваш_ключ_погоды'
 CITY_NAME = 'Moscow'
-URL = f'http://api.openweathermap.org/data/2.5/weather?id={524901}&appid={7473e21}'
+URL = f'http://api.openweathermap.org/data/2.5/weather?id=524901&appid={WEATHER_API_KEY}'
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -18,10 +20,17 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 router = Router()
 
+# Создание папки img, если не существует
+if not os.path.exists('img'):
+    os.makedirs('img')
+
+# Инициализация переводчика
+translator = Translator()
+
 # Команда /start
 @router.message(CommandStart())
 async def send_welcome(message: Message):
-    await message.reply("Добро пожаловать! Я бот, который предоставляет прогноз погоды.\n"
+    await message.reply("Добро пожаловать! Я бот, который предоставляет прогноз погоды и переводит текст на английский.\n"
                         "Используйте /help для получения списка доступных команд.")
 
 # Команда /help
@@ -56,10 +65,16 @@ def get_weather(city_name, api_key):
     else:
         return None
 
-async def main():
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-
-if __name__ == '__main__':
-    asyncio.run(main())
+# Обработчик текстовых сообщений для перевода на английский
+# @router.message()
+# async def translate_to_english(message: Message):
+#     translated = translator.translate(message.text, src='auto', dest='en')
+#     await message.reply(f"Перевод на английский: {translated.text}")
+#
+# async def main():
+#     dp.include_router(router)
+#     await bot.delete_webhook(drop_pending_updates=True)
+#     await dp.start_polling(bot)
+#
+# if __name__ == '__main__':
+#     asyncio.run(main())
